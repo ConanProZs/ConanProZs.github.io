e9997fbbf6acd8e0edc61e44e7ba7907aa2f56c5
ConanProZs <colinden09@gmail.com>
2022-03-04 13:54:50 +0100

    Delete code.js

--- code.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('check').addEventListener('click', function(){
-if (document.getElementById('checkbox').checked) {
-} else {
-    document.getElementById('error').style.visibility = 'visible';
-}
-});
-
-document.getElementById("body").style.display = "none";
