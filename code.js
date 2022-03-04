4c85d79ed8020759fde7d3bdbd38fa1f58c9fae8
ConanProZs <colinden09@gmail.com>
2022-03-04 13:47:03 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
 document.getElementById('check').addEventListener('click', function(){
 if (document.getElementById('checkbox').checked) {
-    window.location.href = "http://www.conansite.net/";
 } else {
     document.getElementById('error').style.visibility = 'visible';
 }
 });
+
+document.getElementById("body").style.display = "none";
---
+++ notchecked.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>verify!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="/main/index.php" method="post">
+    <div>
+    <input type="checkbox" title="verify" id="checkbox" name="verifybox"></div>
+
+    
+    <input type="submit" value="continue" class="con" id="check">
+</form>
+    <br><br><br>
+    <h3 id="error" title="error message">THE CHECKBOX IS NOT CHECKED</h3>
+</body>
+</html>
